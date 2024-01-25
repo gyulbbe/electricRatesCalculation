911c5c39d14440f3650bfc407a89f4239167a87e
HAN <HAN@DESKTOP-8FU98QS>
2024-01-25 20:14:35 +0900

    good

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,4 +1,19 @@
 public class Calculation {
-
-  // code here
+	//싱글톤 패턴
+	private static final SingletonService instance = new SingletonService();
+	//getter
+	public static SingletonService getInstance() {
+		return instance;
+	}
+	//객체 생성 방어
+	private SingletonService() {}
+	
+	//기본요금
+	public int baseFee(int kWh) {
+		//200이하 사용시 910원
+		if(kWh<=200)
+			return 910;
+		//
+		else if(kWh)
+	}
 }
